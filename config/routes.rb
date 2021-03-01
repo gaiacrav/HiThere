1e2b3093bae0cca2b5411eeafbd4e196ee62c34b
Gaia Cravesana <gcravesana@gmail.com>
2021-03-01 22:16:50 +0000

    started working on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :preferences do
+    resources :chatrooms, only: [:create, :show]
+  end
+
+  resources :chatrooms, only: [:show] do
+    resources :messages, only: [:create]
+  end
+
+  resources :users
+
+  resources :preferences do
+    collection do
+      get :matches
+    end
+  end
+
+
+ # get "my_bookings", to: "pages#my_bookings"
 end
