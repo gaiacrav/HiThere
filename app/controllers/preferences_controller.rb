9581b5fd39b6860418189fca9b0b3bbe50cf2444
Gaia Cravesana <gcravesana@gmail.com>
2021-03-03 11:38:00 +0000

    created the matches view

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,10 +1,18 @@
 class PreferencesController < ApplicationController
- def matches
-    # Eliseu: just an idea for a first criteria definition:
-    match_country = current_user.country
-    match_genre = current_user.medium.genre  # Eliseu: not sure about syntax
 
-    @matches = User.where(country: match_country).or(User.where(genre: match_genre))
+  def show
+    raise
+  end
+
+  def matches
+    # Eliseu: just an idea for a first criteria definition:
+    # match_country = current_user.country
+    # match_genre = current_user.medium.genre  # Eliseu: not sure about syntax
+
+    # @matches = User.where(country: match_country).or(User.where(genre: match_genre))
     # Eliseu: Not sure about this - still needs validation from a TA
-end
+
+    @matches = User.all
+  end
+
 end
