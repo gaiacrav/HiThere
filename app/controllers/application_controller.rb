5b1e14d4813c976fed4cea06b7328c5fd694d38f
Gaia Cravesana <gcravesana@gmail.com>
2021-03-04 23:25:56 +0000

    made some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :age, :gender, :country, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
+
+
 end
 
 
