46147396264b4ce32e018e063dfd1f0deb7b7239
Gaia Cravesana <gcravesana@gmail.com>
2021-03-04 12:01:46 +0000

    changes to video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,8 @@
 class Video < ApplicationRecord
-
   belongs_to :user
   has_many :preferences
-
+  has_many :videos_genres
+  has_many :genres, through: :videos_genres
 
   validates :title, presence: true
   validates :genre, inclusion: { in: %w(romantic horror thriller scienceFiction comedy suspence historical documentary others),
