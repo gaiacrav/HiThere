ae8a13145f37f5a10f59900053406cd07de45dd6
Gaia Cravesana <gcravesana@gmail.com>
2021-03-04 12:59:19 +0000

    parse csv done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'faker'
+#require 'faker'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -7,16 +7,27 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
-Video.destroy_all
+#User.destroy_all
+#Video.destroy_all
 
 # Users seed
 
-User.create!(first_name: "Gaia", last_name: "Cravesana", email: "gaia@gmail.com", password: "12345678", gender: "female", age: 26, country: "Italy", admin: true)
-User.create!(first_name: "Sara", last_name: "Subtil", email: "sara@gmail.com", password: "12345678", gender: "female", age: 35, country: "Portugal", admin: true)
-User.create!(first_name: "Eliseu", last_name: "Mateus", email: "eliseu@gmail.com", password: "12345678", gender: "male", age: 50, country: "Portugal", admin: true)
-User.create!(first_name: "Lola", last_name: "Net", email: "lola@gmail.com", password: "12345678", gender: "female", age: 37, country: "Spain", admin: false)
+#User.create!(first_name: "Gaia", last_name: "Cravesana", email: "gaia@gmail.com", password: "12345678", gender: "female", age: 26, country: "Italy", admin: true)
+#User.create!(first_name: "Sara", last_name: "Subtil", email: "sara@gmail.com", password: "12345678", gender: "female", age: 35, country: "Portugal", admin: true)
+#User.create!(first_name: "Eliseu", last_name: "Mateus", email: "eliseu@gmail.com", password: "12345678", gender: "male", age: 50, country: "Portugal", admin: true)
+#User.create!(first_name: "Lola", last_name: "Net", email: "lola@gmail.com", password: "12345678", gender: "female", age: 37, country: "Spain", admin: false)
 
-20.times do
-  movie = Video.create(title: Faker::Movie.title, genre: 'horror', kind: 'movie')
+#20.times do
+  #movie = Video.create(title: Faker::Movie.title, genre: 'horror', kind: 'movie')
+#end
+
+# parse CSV
+require 'csv'
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath = 'db/top1000movies.csv'
+
+CSV.foreach(filepath, csv_options) do |row|
+  puts "#{row["Title"]} | #{row["Genre"]} | #{row["Description"]} | #{row["Year"]} | #{row["Runtime (Minutes)"]}"
 end
+
