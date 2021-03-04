9c7b98226dbfa502f5e732f214ef06e20c8303b4
Sara Subtil <subtilsara026@gmail.com>
2021-03-04 15:48:39 +0000

    fixed a bug regarding the image upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,27 +3,27 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       @users =User.all # Eliseu: This must be changed to filter only friends
     end
-  
+
     # def edit
     #   @user = User.find(params[:id])
     # end
-  
+
     # def update
     #   @user = User.find(params[:id])
     #   @user.update(user_params)
     #   redirect_to user_path
     # end
-  
+
     # def destroy
     #   @user = User.find(params[:id])
     #   @user.admin = current_user
     #   @user.destroy
     #   redirect_to users_path
     # end
-  
+
     private
-  
+
     def user_params
-      params.require(:user).permit(:email, :first_name, :last_name, :country, :age, :gender, :admin )
+      params.require(:user).permit(:email, :first_name, :last_name, :country, :age, :gender, :admin, :photo)
     end
   end
