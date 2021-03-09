ff4b6fedadc10fbe77cdedf2a7ded9add7ec8234
Sara Subtil <subtilsara026@gmail.com>
2021-03-09 10:53:53 +0000

    Fixed the preferences controller and the views so that are no duplicated choices and also there's no opportunity to choose the same movies again, because they have already been chosen, like the heart symbol demonstrates

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+Chatroom.destroy_all
 Preference.destroy_all
 VideosGenre.destroy_all
 Genre.destroy_all
@@ -15,10 +17,10 @@ Video.destroy_all
 
 # Users seed
 
-User.create!(first_name: "Gaia", last_name: "Cravesana", email: "gaia@gmail.com", password: "12345678", gender: "female", age: 26, country: "Italy", admin: true)
-User.create!(first_name: "Sara", last_name: "Subtil", email: "sara@gmail.com", password: "12345678", gender: "female", age: 35, country: "Portugal", admin: true)
-eliseu = User.create!(first_name: "Eliseu", last_name: "Mateus", email: "eliseu@gmail.com", password: "12345678", gender: "male", age: 50, country: "Portugal", admin: true)
-User.create!(first_name: "Lola", last_name: "Net", email: "lola@gmail.com", password: "12345678", gender: "female", age: 37, country: "Spain", admin: false)
+User.create!(first_name: "Gaia", last_name: "Cravesana", email: "gaia@gmail.com", password: "12345678", gender: "female", age: 26, country: "Italy", admin: true, birth_date: Date.today)
+User.create!(first_name: "Sara", last_name: "Subtil", email: "sara@gmail.com", password: "12345678", gender: "female", age: 35, country: "Portugal", admin: true, birth_date: Date.today)
+eliseu = User.create!(first_name: "Eliseu", last_name: "Mateus", email: "eliseu@gmail.com", password: "12345678", gender: "male", age: 50, country: "Portugal", admin: true, birth_date: Date.today)
+User.create!(first_name: "Lola", last_name: "Net", email: "lola@gmail.com", password: "12345678", gender: "female", age: 37, country: "Spain", admin: false, birth_date: Date.today)
 
 
 # parse CSV
@@ -38,15 +40,15 @@ CSV.foreach(filepath, csv_options) do |row|
   end
 end
 
-# 5.times do 
+# 5.times do
 #   user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123456", gender: ["male", "female"].sample, age: 26, country: Faker::Address.country, admin: false)
 #   4.times do
 #     video = Video.all.sample
-#     preference = Preference.create!(user: user, video: video) 
+#     preference = Preference.create!(user: user, video: video)
 #   end
 #   video = Video.all.sample
 #   preference = Preference.create!(user: user, video: video)
-#   preference = Preference.create!(user: eliseu, video: video) 
+#   preference = Preference.create!(user: eliseu, video: video)
 # end
 
 
