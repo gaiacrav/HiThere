100a427b1606964c99c27a3c02fac6e9432149a4
Gaia Cravesana <gcravesana@gmail.com>
2021-03-09 12:21:26 +0000

    working on the chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,11 +3,11 @@ class ChatroomsController < ApplicationController
         @chatroom = Chatroom.find(params[:id])
         @message = Message.new
     end
-    
+
     def create
         @chatroom = Chatroom.new(chatroom_params)
         @chatroom.creator = current_user
-        @chatroom.name = "#{current_user.first_name} - #{@chatroom.user.first_name} "
+        @chatroom.name = "#{current_user.first_name} - #{@chatroom.user.first_name}"
         if @chatroom.save
           redirect_to @chatroom
         else
@@ -16,7 +16,7 @@ class ChatroomsController < ApplicationController
     end
 
     private
-    
+
     def chatroom_params
         params.require(:chatroom).permit(:user_id)
     end
